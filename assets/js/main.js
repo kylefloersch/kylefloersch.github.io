2a93b7e782341664bc9d867d15915975a46fa314
kylefloersch <kylefloersch@yahoo.com>
2020-08-06 13:38:09 -0400

    added navigation tab at top of page and resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,28 @@
 		});
 
 })(jQuery);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function showNavigationItems() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// Get the container element
+var btnContainer = document.getElementById("myTopnav");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
